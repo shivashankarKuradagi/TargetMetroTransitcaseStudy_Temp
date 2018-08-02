8586e6f90d22ec1d67c1c37d8f96009b52ab82ea
shivashankarKuradagi <37844949+shivashankarKuradagi@users.noreply.github.com>
2018-08-02 23:25:05 +0530

    Added SpringBoot Application for MetroTransit Case Study Application
    Added SpringBoot Application for MetroTransit Case Study Application

---
+++ src/main/java/com/target/metrotransit/metrotransitCaseStudy/MetrotransitCaseStudyApplication.java
@@ -0,0 +1,12 @@
+package com.target.metrotransit.metrotransitCaseStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrotransitCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetrotransitCaseStudyApplication.class, args);
+	}
+}
